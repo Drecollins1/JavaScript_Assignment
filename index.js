9af1a0706ed9c1a191cbc2fbc7b4777cd362c85e
Drecollins1 <115148885+Drecollins1@users.noreply.github.com>
2024-01-30 20:55:46 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// Task 1 & 2: Declare two variables, one for age and one for city and assign it a value.
+let age = 25;
+let city = "Lagos State";
+
+//  Task 3: I used the console log method to display this information on the browser's console
+console.log(
+  "My name is Damilare i am " + age + " years old, " + "and i live in " + city + "."
+);
+//  task 4: The data type i assigned to my "age" Variable is a Number data type and the one for the "city" is a string data type.
+
+// Task 5: Create three more variables, and give them each different data types  (number, string, boolean).
+let height = 170;
+let job = "Front-End Developer";
+let isMarried = false;
+
+// Task 6: I used the console log method to display this information on the browser's console
+console.log(height);
+console.log(job);
+console.log(isMarried);
